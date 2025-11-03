d5f656e54c73d25be569301babce46cc38d33c4f
David Tjomsland <85601135+DTjomsland@users.noreply.github.com>
2025-11-03 12:13:23 +0800

    working on main landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,7 @@ export default {
         accent: 'var(--aw-color-accent)',
         default: 'var(--aw-color-text-default)',
         muted: 'var(--aw-color-text-muted)',
+        'bg-secondary': '#EEF1F0',
       },
       fontFamily: {
         sans: ['var(--aw-font-sans, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
